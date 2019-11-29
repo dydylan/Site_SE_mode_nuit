b0f1d908b0d33dea16b2dc5ed13a7516c5a1d661
dydylan <53371447+dydylan@users.noreply.github.com>
2019-11-29 13:47:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let switch = document.querySelector('#switch');
 let sun;
 
 document.addEventListener('DOMContentLoaded', () => {
-    sun = localStorage.getItem('day');
+    sun = localStorage.getItem('sun');
  if(sun == undefined){
     document.body.setAttribute('data-theme', 'light');
     sun = false; 
@@ -17,11 +17,11 @@ switch.addEventListener('click', () => {
     if(!sun) {
         document.body.setAttribute('data-theme', 'dark');
         sun = true;
-        localStorage.setItem('day', 'false');
+        localStorage.setItem('sun', 'false');
     }
     else{
         sun = false;
         document.body.setAttribute('data-theme', 'light');
-        localStorage.removeItem('day');
+        localStorage.removeItem('sun');
     }
 });
